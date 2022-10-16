28463d023d213411788d2b4e168dbd87bd1a0acc
Lofkvist <carl.lofkvist@gmail.com>
2022-10-16 18:21:09 +0200

    Code readability improved

--- body.py
+++ body.py
@@ -16,118 +16,108 @@ PINK = (226, 99, 149)
 GREEN = (124,252,0)
 PURPLE = (163, 22, 250)
 ORANGE = (252, 140, 45)
-TRAIL_COLORS = [PINK, GREEN, PURPLE, ORANGE, RED]
+MINT = (204, 255, 229)
+TRAIL_COLORS = [MINT, PINK, GREEN, PURPLE, ORANGE, RED]
 
 FPS = 70
 VEL_CONST = 0.02
 
 class Body():
+    def __init__(self, x, y, radius, mass, x_vel, y_vel, trail_color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.mass = mass
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.decay_const = self.mass*10 ** -9
 
-  def __init__(self, x, y, radius, mass, x_vel, y_vel, trail_color):
-    self.x = x
-    self.y = y
-    self.radius = radius
-    self.mass = mass
-    self.x_vel = x_vel
-    self.y_vel = y_vel
-    self.decay_const = self.mass*10 ** -8
+        self.trail_color = trail_color
+        self.trail = []
+        self.time_of_creation = 0
 
-    self.trail_color = trail_color
-    self.trail = []
-    self.time_of_creation = 0
-
-  def drawBody(self):
-    pygame.draw.circle(WIN, WHITE, (self.x, self.y), self.radius, 0)
+    def drawBody(self):
+        pygame.draw.circle(WIN, self.trail_color, (self.x, self.y), self.radius, 0)
   
-  def drawVelVector(self):
-    pygame.draw.line(WIN, RED, (self.x, self.y), (self.x + 0.2*self.x_vel, self.y + 0.2*self.y_vel))
+    def drawVelVector(self):
+        pygame.draw.line(WIN, RED, (self.x, self.y), (self.x + 0.2*self.x_vel, self.y + 0.2*self.y_vel))
 
-  def addTrailpoint(self):
-    self.trail.append((self.x, self.y))
+    def addTrailpoint(self):
+        self.trail.append((self.x, self.y))
 
-    if len(self.trail) > 50:
-      self.trail = self.trail[-50:-1]
+        if len(self.trail) > 50:
+            self.trail = self.trail[-50:-1]
 
-  def massDecay(self, frame, bodies):
-    self.radius = self.radius * math.e ** ( -self.decay_const * (frame - self.time_of_creation) )
-    self.mass = self.mass * math.e ** ( -self.decay_const * (frame - self.time_of_creation) )
-    self.decay_const = self.mass*10 ** -8
+    def massDecay(self, frame, bodies):
+        self.radius = self.radius * math.e ** ( -self.decay_const * (frame - self.time_of_creation) )
+        self.mass = self.mass * math.e ** ( -self.decay_const * (frame - self.time_of_creation) )
+        self.decay_const = self.mass*10 ** -9
 
-    if self.mass < 1 or self.radius < 2:
-      bodies.remove(self)
+        if self.mass < 1 or self.radius < 2:
+            bodies.remove(self)
 
-  def drawTrail(self):
-    for p in self.trail:
-      pygame.draw.circle(WIN, self.trail_color, (p[0], p[1]), 1, 0)
+    def drawTrail(self):
+        for p in self.trail:
+            pygame.draw.circle(WIN, self.trail_color, (p[0], p[1]), 1, 0)
   
-  def drawData(self, font):
-    mass_text = font.render(f'Mass: {round(self.mass)} kg', False, WHITE)
-    vel_text = font.render(f'Velocity: {round(math.sqrt(self.x_vel**2 + self.y_vel**2)):,} m/s', False, WHITE)
+    def drawData(self, font):
+        mass_text = font.render(f'Mass: {round(self.mass)} kg', False, WHITE)
+        vel_text = font.render(f'Velocity: {round(math.sqrt(self.x_vel**2 + self.y_vel**2)):,} m/s', False, WHITE)
 
-    WIN.blit(mass_text, (self.x + self.radius, self.y - self.radius))
-    WIN.blit(vel_text, (self.x + self.radius, self.y - self.radius - 20))
+        WIN.blit(mass_text, (self.x + self.radius, self.y - self.radius))
+        WIN.blit(vel_text, (self.x + self.radius, self.y - self.radius - 20))
 
 
-  def collision(self, other, bodies):
-    if round(self.x_vel, 3) == round(other.x_vel, 3) and round(self.y_vel, 3) == round(other.y_vel, 3):
-      if self.mass > other.mass:
-        self.mass += other.mass
-        self.radius += math.sqrt(other.mass)
-        bodies.remove(other)
-      else:
-        other.mass += self.mass
-        other.radius += math.sqrt(self.radius)
-        bodies.remove(self)
+    def collision(self, other): # Fully Elastic collision
+        m1 = self.mass
+        m2 = other.mass
 
-    m1 = self.mass
-    m2 = other.mass
+        v_1 = np.array([self.x_vel, self.y_vel])
+        pos_1 = np.array([self.x, self.y])
 
-    v_1 = np.array([self.x_vel, self.y_vel])
-    pos_1 = np.array([self.x, self.y])
+        v_2 = np.array([other.x_vel, other.y_vel])
+        pos_2 = np.array([other.x, other.y])
 
-    v_2 = np.array([other.x_vel, other.y_vel])
-    pos_2 = np.array([other.x, other.y])
+        v_1_new = v_1 - (pos_1 - pos_2) * (2 * m2/(m1 + m2)) * np.inner(v_1 - v_2, pos_1 - pos_2) / (np.inner(pos_1 - pos_2, pos_1 - pos_2)) 
+        v_2_new = v_2 - (pos_2 - pos_1) * (2 * m1/(m1 + m2)) * np.inner(v_2 - v_1, pos_2 - pos_1) / (np.inner(pos_2 - pos_1, pos_2 - pos_1))
 
-    v_1_new = v_1 - (pos_1 - pos_2) * (2 * m2/(m1 + m2)) * np.inner(v_1 - v_2, pos_1 - pos_2) / (np.inner(pos_1 - pos_2, pos_1 - pos_2)) 
-    v_2_new = v_2 - (pos_2 - pos_1) * (2 * m1/(m1 + m2)) * np.inner(v_2 - v_1, pos_2 - pos_1) / (np.inner(pos_2 - pos_1, pos_2 - pos_1))
-    
-    self.x_vel, self.y_vel = v_1_new
-    other.x_vel, other.y_vel = v_2_new
+        self.x_vel, self.y_vel = v_1_new
+        other.x_vel, other.y_vel = v_2_new
 
-    self.x += self.x_vel*(1/FPS)
-    self.y += self.y_vel*(1/FPS)
-    other.x += other.x_vel*(1/FPS)
-    other.y += other.y_vel*(1/FPS)
+        self.x += self.x_vel*(1/FPS)
+        self.y += self.y_vel*(1/FPS)
+        other.x += other.x_vel*(1/FPS)
+        other.y += other.y_vel*(1/FPS)
 
-  def updatePosition(self):
-    self.x += self.x_vel*(1/FPS)
-    self.y += self.y_vel*(1/FPS)
+    def updatePosition(self):
+        self.x += self.x_vel*(1/FPS)
+        self.y += self.y_vel*(1/FPS)
 
-  def updateVelocity(self, bodies, G, collison_bool):
-    for other in bodies:
-      if other == self:
-        continue
+    def updateVelocity(self, bodies, G, collison_bool):
+        for other in bodies:
+            if other == self:
+                continue
 
-      else:
-        # Gravitational force from other bodies
-        dX = self.x - other.x
-        dY = self.y - other.y
-        r = math.sqrt((dX)**2 + (dY)**2)
-        
-        dX_next = (self.x + self.x_vel*(1/FPS)) - (other.x + other.x_vel*(1/FPS))
-        dY_next = (self.y + self.y_vel*(1/FPS)) - (other.y + other.y_vel*(1/FPS))
-        r_next = math.sqrt((dX_next)**2 + (dY_next)**2)
+            else:
+                # Gravitational force from other bodies
+                dX = self.x - other.x
+                dY = self.y - other.y
+                r = math.sqrt((dX)**2 + (dY)**2)
 
-        # Check for collision in next frame
-        if r_next <= self.radius + other.radius and collison_bool:
-          self.collision(other, bodies)
+                dX_next = (self.x + self.x_vel*(1/FPS)) - (other.x + other.x_vel*(1/FPS))
+                dY_next = (self.y + self.y_vel*(1/FPS)) - (other.y + other.y_vel*(1/FPS))
+                r_next = math.sqrt((dX_next)**2 + (dY_next)**2)
 
-        else:
-          F = -G * (self.mass * other.mass) / (r**2)
-          
-          theta = math.atan2(dY, dX)
-          x_acc = math.cos(theta) * F/self.mass
-          y_acc = math.sin(theta) * F/self.mass
+                # Check for collision in next frame
+                if r_next <= self.radius + other.radius and collison_bool:
+                    self.collision(other)
 
-          self.x_vel += 2*x_acc*(1/FPS)
-          self.y_vel += 2*y_acc*(1/FPS)
+                else:
+                    F = -G * (self.mass * other.mass) / (r**2)
+
+                    theta = math.atan2(dY, dX)
+                    x_acc = math.cos(theta) * F/self.mass
+                    y_acc = math.sin(theta) * F/self.mass
+
+                    self.x_vel += 2*x_acc*(1/FPS)
+                    self.y_vel += 2*y_acc*(1/FPS)
--- simulation.py
+++ simulation.py
@@ -12,6 +12,7 @@ WINDOW_HEIGHT = 700
 NAVBAR_HEIGHT = 100
 FPS = 70
 BACKGROUND = pygame.image.load("space_bg.png")
+pygame.display.set_caption('Newtonian Mechanics simulator')
 WIN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 VEL_CONST = 0.02
 prefix = ['', 'k', 'M', 'G', 'T', 'P']
@@ -25,165 +26,164 @@ PINK = (226, 99, 149)
 GREEN = (124,252,0)
 PURPLE = (163, 22, 250)
 ORANGE = (252, 140, 45)
-TRAIL_COLORS = [PINK, GREEN, PURPLE, ORANGE, RED]
-
+MINT = (204, 255, 229)
+TRAIL_COLORS = [MINT, PINK, ORANGE, PURPLE, GREEN, RED]
 
 def totalEnergy(bodies):
-  E = 0
-  for body in bodies:
-    v = math.sqrt(body.x_vel**2 + body.y_vel**2)
-    E += (body.mass/2) * (v ** 2)
-  return round(E, 2)
+	E = 0
+	for body in bodies:
+		v = math.sqrt(body.x_vel**2 + body.y_vel**2)
+		E += (body.mass/2) * (v ** 2)
+	return round(E, 2)
 
 
 def inflate(x, y, time): # Inflate body
-  pygame.draw.circle(WIN, WHITE, (x, y), 10*time, 0)
+	pygame.draw.circle(WIN, WHITE, (x, y), 10*time, 0)
 
 
 # Main game loop
 def main():
-  clock = pygame.time.Clock()
-  bodies = []
-  frame = 0
-  draw_timer = 0
-  G = 40
+	clock = pygame.time.Clock()
+	bodies = []
+	frame = 0
+	draw_timer = 0
+	G = 40
 
-  trail_shown = True
-  drawing = False
-  decay_active = False
-  collision_absorb = True
-  pause = False
+	TRAIL_SHOWN = True
+	DRAWING = False
+	DECAY_ACTIVE = False
+	COLLISION_ABSORB = True
+	PAUSE = False
 
-  font = pygame.font.SysFont('Helvetica', 14)
-  bigFont = pygame.font.SysFont('Helvetica', 30)
-  font.set_bold(True)
-  bigFont.set_bold(True)
+	font = pygame.font.SysFont('Helvetica', 14)
+	bigFont = pygame.font.SysFont('Helvetica', 30)
+	font.set_bold(True)
+	bigFont.set_bold(True)
 
-  game_header = bigFont.render('Simulation of Newtonian Mechanics', False, BLUE)
-  clear_text = font.render('Clear simulation:   [R]', False, BLUE)
-  trail_toggle = font.render('Toggle trail:          [T]', False, BLUE)
-  grav_const = font.render(f'Toggle G by 100:                                   [+/-]', False, BLUE)
-  decay_text = font.render('Toggle mass decay:                               [D]', False, BLUE)
-  collision_absorb_text = font.render('Toggle ellastic collision and absorbtion:  [C]', False, BLUE)
-  pause_text = font.render('Pause simulation:  [P]', False, BLUE)
-  while_paused_text = bigFont.render('PAUSED', False, BLUE)
+	game_header = bigFont.render('Simulation of Newtonian Mechanics', False, BLUE)
+	clear_text = font.render('Clear simulation:   [R]', False, BLUE)
+	trail_toggle = font.render('Toggle trail:          [T]', False, BLUE)
+	grav_const = font.render(f'Toggle G by 100:                                   [+/-]', False, BLUE)
+	decay_text = font.render('Toggle mass decay:                               [D]', False, BLUE)
+	collision_absorb_text = font.render('Toggle ellastic collision and absorbtion:  [C]', False, BLUE)
+	pause_text = font.render('Pause simulation:  [P]', False, BLUE)
+	while_paused_text = bigFont.render('PAUSED', False, BLUE)
 
 
-  while True:
-    clock.tick(FPS)
-    WIN.blit(BACKGROUND, (0,0))
-    pygame.display.set_caption('Gravity simulator')
-    pygame.draw.rect(WIN, WHITE, pygame.Rect(0, 0, WINDOW_WIDTH, NAVBAR_HEIGHT))
+	while True:
+		clock.tick(FPS)
+		WIN.blit(BACKGROUND, (0,0))
+		pygame.draw.rect(WIN, WHITE, pygame.Rect(0, 0, WINDOW_WIDTH, NAVBAR_HEIGHT))
 
-    E = totalEnergy(bodies)
-    i = 0
-    while E > 1000:
-      E = E/1000
-      i += 1
+		E = totalEnergy(bodies)
+		i = 0
+		while E > 1000:
+			E = E/1000
+			i += 1
+		pre = prefix[i]
+		energy_text = font.render(f'Total kinetic energy: {round(E, 4)} {pre}J', False, BLUE)
 
-    pre = prefix[i]
-    energy_text = font.render(f'Total kinetic energy: {round(E, 4)} {pre}J', False, BLUE)
+		#Display text
+		WIN.blit(game_header, (10, 5))
 
-    #Display text
-    WIN.blit(game_header, (10, 5))
+		WIN.blit(clear_text, (10, 40))
+		WIN.blit(trail_toggle, (10, 60))
+		WIN.blit(pause_text, (10, 80))
 
-    WIN.blit(clear_text, (10, 40))
-    WIN.blit(trail_toggle, (10, 60))
-    WIN.blit(pause_text, (10, 80))
+		WIN.blit(collision_absorb_text, (270, 40))
+		WIN.blit(decay_text, (270, 60))
+		WIN.blit(grav_const, (270, 80))
 
-    WIN.blit(collision_absorb_text, (270, 40))
-    WIN.blit(decay_text, (270, 60))
-    WIN.blit(grav_const, (270, 80))
+		WIN.blit(energy_text, (900, 80))
 
-    WIN.blit(energy_text, (900, 80))
+		frame += 1
 
-    frame += 1
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
 
-    for event in pygame.event.get():
-      if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				mouse_start = pygame.mouse.get_pos()
+				DRAWING = True
 
-      elif event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_start = pygame.mouse.get_pos()
-        drawing = True
-
-      elif event.type == pygame.MOUSEBUTTONUP:
-        mouse_end = pygame.mouse.get_pos()
-        drag = mouse_start[0] - mouse_end[0], mouse_start[1] - mouse_end[1]
-        x_vel, y_vel = drag
+			elif event.type == pygame.MOUSEBUTTONUP:
+				mouse_end = pygame.mouse.get_pos()
+				drag = mouse_start[0] - mouse_end[0], mouse_start[1] - mouse_end[1]
+				x_vel, y_vel = drag
         
-        #Add drawn body
-        if draw_timer > 0.5:
-          bodies.append(
-            Body(mouse_start[0], mouse_start[1], 10*draw_timer, 10*draw_timer**3,
-          x_vel, y_vel, TRAIL_COLORS[len(bodies)%len(TRAIL_COLORS)]))
-          bodies[-1].time_of_creation = frame
-        draw_timer = 0
-        drawing = False
+				#Add drawn body
+				if draw_timer > 0.5:
+					bodies.append(
+					Body(mouse_start[0], mouse_start[1], 10*draw_timer, 10*draw_timer ** 4,
+					x_vel, y_vel, TRAIL_COLORS[len(bodies)%len(TRAIL_COLORS)]))
+
+					bodies[-1].time_of_creation = frame
+
+				draw_timer = 0
+				DRAWING = False
       
-      #Keyboard input
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_r:
-          bodies.clear()
+			#Keyboard input
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					bodies.clear()
 
-        elif event.key == pygame.K_t:
-          trail_shown = not trail_shown
+				elif event.key == pygame.K_t:
+					TRAIL_SHOWN = not TRAIL_SHOWN
 
-        elif event.key == pygame.K_PLUS:
-          G += 100
+				elif event.key == pygame.K_PLUS:
+					G += 100
 
-        elif event.key == pygame.K_MINUS:
-          G -= 100
-          if G < 0:
-            G = 0
-        elif event.key == pygame.K_p:
-          pause = not pause
+				elif event.key == pygame.K_MINUS:
+					G -= 100
+					if G < 0:
+						G = 0
+				elif event.key == pygame.K_p:
+					PAUSE = not PAUSE
 
-        elif event.key == pygame.K_d:
-          decay_active = not decay_active
-          
-        elif event.key == pygame.K_c:
-          collision_absorb = not collision_absorb
+				elif event.key == pygame.K_d:
+					DECAY_ACTIVE = not DECAY_ACTIVE
+	
+				elif event.key == pygame.K_c:
+					COLLISION_ABSORB = not COLLISION_ABSORB
 
-    if drawing:
-      draw_timer += 1/FPS
-      inflate(mouse_start[0], mouse_start[1], draw_timer)
+		if DRAWING:
+			draw_timer += 1/FPS
+			inflate(mouse_start[0], mouse_start[1], draw_timer)
 
-    # Update data of bodies
-    for body in bodies:
-      if pause:
-        WIN.blit(while_paused_text, (WINDOW_WIDTH//2 - 30, WINDOW_HEIGHT//2))
-        body.drawBody()
-        body.drawVelVector()
-        body.drawData(font)
-        if trail_shown:
-          body.drawTrail()
-        continue
+		# Update data of bodies
+		for body in bodies:
+			if PAUSE:
+				WIN.blit(while_paused_text, (WINDOW_WIDTH//2 - 30, WINDOW_HEIGHT//2))
+				body.drawBody()
+				body.drawVelVector()
+				body.drawData(font)
+				if TRAIL_SHOWN:
+					body.drawTrail()
+				continue
 
-      body.updateVelocity(bodies, G,collision_absorb)
-      body.updatePosition()
-      body.drawBody()
-      body.drawVelVector()
-      body.drawData(font)
+			body.updateVelocity(bodies, G, COLLISION_ABSORB)
+			body.updatePosition()
+			body.drawBody()
+			body.drawVelVector()
+			body.drawData(font)
 
-      if frame%(FPS//8) == 1:
-        body.addTrailpoint()
+			if frame%(FPS//8) == 1:
+				body.addTrailpoint()
 
-      if trail_shown:
-        body.drawTrail()
+			if TRAIL_SHOWN:
+				body.drawTrail()
       
-      if decay_active:
-        body.massDecay(frame, bodies)
+			if DECAY_ACTIVE:
+				body.massDecay(frame, bodies)
 
-      # Bounce of walls
-      if body.y + body.y_vel*(1/FPS) - body.radius < NAVBAR_HEIGHT or body.y + body.y_vel*(1/FPS) + body.radius > WINDOW_HEIGHT:
-        body.y_vel = -body.y_vel
+			# Bounce of walls
+			if body.y + body.y_vel*(1/FPS) - body.radius < NAVBAR_HEIGHT or body.y + body.y_vel*(1/FPS) + body.radius > WINDOW_HEIGHT:
+				body.y_vel = -body.y_vel
 
-      if body.x + body.x_vel*(1/FPS) - body.radius < 0 or body.x + body.x_vel*(1/FPS) + body.radius > WINDOW_WIDTH:
-        body.x_vel = -body.x_vel
-
-    pygame.display.update()
+			if body.x + body.x_vel*(1/FPS) - body.radius < 0 or body.x + body.x_vel*(1/FPS) + body.radius > WINDOW_WIDTH:
+				body.x_vel = -body.x_vel
+		pygame.display.update()
 
 if __name__ == '__main__':
   main()
